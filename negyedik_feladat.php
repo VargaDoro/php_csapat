88b297802423d781d390772e180f877c9bea1cc8
VargaDoro <varga.dorothea.timea@gmail.com>
2025-02-07 10:25:16 +0100

    negyedik

--- negyedik_feladat.php
+++ negyedik_feladat.php
@@ -6,8 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Hogyan takarékoskodhatunk a vízzel?</h1>
     <?php
     echo "<p><a href='index.php'>Főoldal</a></p>";
+    include 'fuggvenyek.php';
+    cim("A Föld édesvízkészlete véges, hogyan takarékoskodhatunk a vízzel?");
+    echo "<p>Az ivóvíz születéséért rengeteget dolgoznak a vízcseppek és az emberek is. Ráadásul a felhasználható víz mennyisége
+    sem korlátlan. Ezért mondják a víztörténetét ismerő felnőttek, hogy óvd a természetet és takarékoskodj a vízzel. Fürdés helyett
+    inkább zuhanyozz! Egészségesebb és takarékosabb is, mint a kádban fürdés. Ne folyasd feleslegesen a vizet, a 
+    fogmosáshoz használj poharat! Ha csöpögő csapot látsz, szólj a felnőtteknek! A mosógépet rakjuk tele ruhával, ha beindítjuk, 
+    egy-két pólóért pazarlás beindítani. De azzal is vizet spórolunk meg, ha vigyázunk a ruháink tisztáságára. Ha kevesebb ruhát 
+    piszkolunk össze, kevesebb lesz a mosnivalónk is. Ügyeljünk arra, hogy sáros lábbal ne menjünk be a lakásba, mert úgy is 
+    megspórolhatunk vizet, ha nem kell naponta többször felmosnunk. A kertet reggel és este locsold, 
+    ilyenkor kevésbé párolog a víz. De locsolásra használhatjuk az összegyűjtött esővizet is. A vizet a növények gyökeréhez juttasd!”</p>";
+    echo "<br>";
+    echo "<p>A folyadékáram jellemző adata a folyadék áramlási sebessége (v). Az áramló folyadék mennyiségét a térfogatárammal fejezzük 
+    ki. A térfogatáram egy adott keresztmetszeten időegység alatt átáramló folyadék térfogata. A térfogatáram számítása: V = v × A</p>"
     ?>
 </body>
 </html>
